98faf02c58784e4eab7a21b0c2e308a46abeec7b
NamSangwon <nsw3265@naver.com>
2025-10-20 17:21:34 +0900

    Add : Open Feign 종속성 추가

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -1,3 +1,4 @@
+val springCloudVersion by extra("2024.0.2")
 tasks.jar {
     enabled = true
 }
@@ -10,4 +11,10 @@ dependencies {
     implementation(projects.modules.application)
     implementation(projects.modules.domain)
     implementation(libs.spring.boot.starter.web)
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
